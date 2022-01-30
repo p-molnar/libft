8e98b0708aadbccf2e2749934642ce318b4ba311
Péter Molnár <pmolnar@Peters-MacBook-Pro.local>
2022-01-30 15:54:16 +0100

    removed null protection

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,14 +6,21 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:17 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:57:03 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/01/30 15:53:20 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+//	NAME
+//		bzero -- write zeroes to a byte string
+//
+//	DESCRIPTION
+//		The bzero() function writes n zeroed bytes to the string s. 
+//		If n is zero, bzero() does nothing.
 
 #include <stddef.h>
 
 void	ft_bzero(void *s, size_t n)
 {
-	while (s && n--)
+	while (n--)
 		*(char *)s++ = '\0';
 }
