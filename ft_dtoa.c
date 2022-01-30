d000885cd99b458ad11f6806927469d88216dd59
Péter Molnár <pmolnar@Peters-MacBook-Pro.local>
2022-01-30 15:22:48 +0100

    added function description and protection

--- ft_dtoa.c
+++ ft_dtoa.c
@@ -6,13 +6,25 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:21 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/01/30 15:17:15 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+//	NAME
+//		dtoa -- converts digit to ascii character
+
+//	DESCRIPTION
+//		ft_dtoa() function converts a digit to its corresponding ascii character
+
+//	RETURN VALUES
+//		if the conversion successfull then the function returns the converted 
+//		charater, otherwise, a null terminator.
 
 #include "libft.h"
 
 char	ft_dtoa(int d)
 {
-	return (d + '0');
+	if (ft_isdigit(d))
+		return (d + CHAR_ZERO);
+	return ('\0');
 }
