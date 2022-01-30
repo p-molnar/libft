bd59beb6fb8b6e0284c849c048fbe40508642c84
Péter Molnár <pmolnar@Peters-MacBook-Pro.local>
2022-01-30 15:05:27 +0100

    added description and changed var naming

--- ft_count.c
+++ ft_count.c
@@ -6,20 +6,34 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:20 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/01/30 14:55:19 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_count(char const *s, char c)
-{
-	unsigned int	i;
+//	NAME
+// 		ft_count -- counts character occurance in string
+//
+//	DESCRIPTION
+// 		ft_count() function counts character c's occurance 
+//		in a string pointed by s. The function counts the null 
+//		character as well.
+//
+//	RETURN VALUE
+// 		ft_count() function returns an int, the number of 
+//		occurance c was found in s.
 
-	i = 0;
+#include "libft.h"
+
+size_t	ft_count(char *s, char c)
+{
+	size_t	count;
+
+	count = 0;
 	while (*s)
 	{
-		i += (*s == c);
+		count += (*s == c);
 		s++;
 	}
-	i += (*s == c);
-	return (i);
+	count += (*s == c);
+	return (count);
 }
