c86594945d1b4c7ac23aeb4e63c7d6458901b5ef
Péter Molnár <pmolnar@Peters-MacBook-Pro.local>
2022-01-30 15:57:10 +0100

    added description to file

--- ft_abs.c
+++ ft_abs.c
@@ -6,11 +6,20 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/24 23:20:01 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/24 23:20:03 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/01/30 13:05:43 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_abs(int n)
+//	NAME																		
+//   	abs -- integer absolute value function
+//
+// 	DESCRIPTION
+//    	The abs() function computes the absolute value of the integer i.
+//
+// 	RETURN VALUES
+//    	The abs() function returns the absolute value.
+
+int	ft_abs(int i)
 {
-	return (n * ((n > 0) - (n < 0)));
+	return (i * ((i > 0) - (i < 0)));
 }
