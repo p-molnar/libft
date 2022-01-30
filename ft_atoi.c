9c77c2677912cf04199b71de3f9141f00fb79713
Péter Molnár <pmolnar@Peters-MacBook-Pro.local>
2022-01-30 15:14:21 +0100

    removed macroes and defined it in the header instead

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,21 +6,28 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:09 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/01/30 15:09:11 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+//	NAME
+//		atoi, atoi_l -- convert ASCII string to integer
+// 
+//	DESCRIPTION
+//		The atoi() function converts the initial portion of the string pointed
+//		to by str to int representation.
+// 
+//	RETURN VALUES
+//		The atoi() function returns a portion of the string that has been 
+//		converted to int.
+
 #include "libft.h"
-#include <stdio.h>
-#define CHAR_PLUS '+'
-#define CHAR_MINUS '-'
-#define CHAR_ZERO '0'
 
 int	ft_atoi(const char *str)
 {
 	int			num;
 	int			coeff;
-	const int	sign[2] = {1, -1};
+	const int	sign[2] = {+1, -1};
 
 	num = 0;
 	coeff = 1;
