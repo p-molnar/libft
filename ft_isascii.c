d487dc12bb687118a4a730d4f17528fff2e8b03d
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-01-31 17:17:43 +0100

    added description to file, and specified int 127 as octal 0177

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,11 +6,18 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:26 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/01/31 17:16:38 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+//	NAME
+//		isascii -- test for ASCII character
+//
+//	DESCRIPTION
+//		The isascii() function tests for an ASCII character, which is any 
+//		character between 0 and octal 0177 inclusive.
+
 int	ft_isascii(int c)
 {
-	return (c >= 0 && c <= 127);
+	return (c >= 0 && c <= 0177);
 }
