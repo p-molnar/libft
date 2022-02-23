60ef99e77e9fc14dbdbdeb494185e762415dcf0f
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-02-23 16:29:44 +0100

    added function description

--- libc/ft_memset.c
+++ libc/ft_memset.c
@@ -6,9 +6,19 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:56 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/02/23 16:29:26 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+//	NAME
+//		memset -- fill a byte string with a byte value
+//	
+//	DESCRIPTION
+//		The memset() function writes len bytes of value c (converted to an
+//		unsigned char) to the string b.
+//	
+//	RETURN VALUES
+//		The memset() function returns its first argument.
 
 #include <stddef.h>
 
