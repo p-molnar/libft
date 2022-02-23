b3a4a060b866fdb9ca4546364eeeee710cd29999
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-02-23 16:09:36 +0100

    added description to function

--- libc/ft_memmove.c
+++ libc/ft_memmove.c
@@ -6,9 +6,20 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:54 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/02/23 15:59:18 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+//	NAME
+//		memmove -- copy byte string
+//
+//	DESCRIPTION
+//		The memmove() function copies len bytes from string src to string dst.
+//		The two strings may overlap; the copy is always done in 
+//		a non-destructive manner.
+//
+//	RETURN VALUES
+//		The memmove() function returns the original value of dst.
 
 #include <stddef.h>
 
