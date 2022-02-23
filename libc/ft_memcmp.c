dfce4cec8c4c2d5eab8ef20a2d6059f5986ee915
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-02-23 15:46:54 +0100

    added func description

--- libc/ft_memcmp.c
+++ libc/ft_memcmp.c
@@ -6,9 +6,24 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:51 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/02/23 15:46:06 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+//	NAME
+//		memcmp -- compare byte string
+//	 
+//	DESCRIPTION
+//		The memcmp() function compares byte string s1 against byte string s2.
+//		Both strings are assumed to be n bytes long.
+//
+//	RETURN VALUES
+//		The memcmp() function returns zero if the two strings are identical, 
+//		otherwise returns the difference between the first two differing bytes
+//		(treated as unsigned char values, so that `\200' is greater than `\0',
+//		for example).  Zero-length strings are always identical.  This behavior
+//		is not required by C and portable code should only depend on the sign of
+//		the returned value
 
 #include <stddef.h>
 
