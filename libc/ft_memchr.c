89224467292359770421a36de0c4fc9c70b42f56
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-02-23 15:43:57 +0100

    added func description

--- libc/ft_memchr.c
+++ libc/ft_memchr.c
@@ -6,9 +6,20 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:49 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/02/23 15:43:28 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+//	NAME
+//		memchr -- locate byte in byte string
+//
+//	DESCRIPTION
+//		The memchr() function locates the first occurrence of c (converted
+//		to an unsigned char) in string s.
+//
+//	RETURN VALUES
+//		The memchr() function returns a pointer to the byte located, or 
+//		NULL if no such byte exists within n bytes.
 
 #include <stddef.h>
 
