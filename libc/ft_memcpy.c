a2cc7ef8ca670b5e9506f475cff0a37187b3a78e
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-02-23 15:51:10 +0100

    added description to function

--- libc/ft_memcpy.c
+++ libc/ft_memcpy.c
@@ -6,9 +6,19 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:52 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/02/23 15:50:03 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+//	NAME
+//		memcpy -- copy memory area
+//
+//	DESCRIPTION
+//		The memcpy() function copies n bytes from memory area src to memory
+//		area dst. If dst and src overlap, behavior is undefined.
+//
+//	RETURN VALUES
+//		The memcpy() function returns the original value of dst.
 
 #include <stddef.h>
 
