6f78064a44a9fdb06c9b7938d65c3a969d8e49ee
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-02-23 16:44:01 +0100

    added function description

--- libc/ft_putchar_fd.c
+++ libc/ft_putchar_fd.c
@@ -6,9 +6,18 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:51:57 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/02/23 16:43:01 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+//	NAME
+//		putchar -- output a character to a specified stream
+//
+//	DESCRIPTION
+//		Outputs the character ’c’ to the given file descriptor.
+//
+//	RETURN VALUES
+//		The function does not have a return value
 
 #include <unistd.h>
 
