87c858a7c585bc4d69045973e9421a4c4e5a6649
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-02-23 16:44:26 +0100

    added function description

--- libc/ft_putstr_fd.c
+++ libc/ft_putstr_fd.c
@@ -6,9 +6,18 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:52:02 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/02/23 16:42:41 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+//	NAME
+//		putstr -- output a string to a specified stream
+//
+//	DESCRIPTION
+//		Outputs the string ’s’ to the given file descriptor.
+//
+//	RETURN VALUES
+//		The function does not have a return value
 
 #include <unistd.h>
 #include "libft.h"
