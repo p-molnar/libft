2ac4598dab68141ea7ea8b66283376c241518373
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-03-08 18:32:50 +0100

    added function description

--- libc/ft_strlcat.c
+++ libc/ft_strlcat.c
@@ -6,12 +6,32 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 17:52:15 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/29 22:58:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/03/08 18:31:09 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+//	NAME
+//		strlcat -- size-bounded string copying and concatenation
+//	
+//	DESCRIPTION
+//		strlcat() take the full size of the destination buffer and
+//		guarantee NUL-termination if there is room.  Note that room for the NUL
+//	    should be included in dstsize.
+//	
+//		strlcat() appends string src to the end of dst.  It will append at most
+//		dstsize - strlen(dst) - 1 characters.  It will then NUL-terminate, 
+//		unless dstsize is 0 or the original dst string was longer than dstsize 
+//		(in practice this should not happen as it means that either dstsize is 
+//		incorrect or that dst is not a proper string).
+//		
+//		If the src and dst strings overlap, the behavior is undefined.
+//	
+//	RETURN VALUES
+//		strlcat() functions return the total length of the string they tried 
+//		to create. For strlcat() that means the initial length of dst plus 
+//		the length of src.
+
 #include "libft.h"
-#include <stddef.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
