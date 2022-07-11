4b96f66b405cbfd3b0211db73b21810e3119e789
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-11 20:46:18 +0200

    merge multiple separate function into one file

--- src/math/ft_max.c
+++ src/math/ft_max.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_max_i.c                                         :+:    :+:            */
+/*   ft_max.c                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/07 18:34:03 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/06/01 15:45:47 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/11 20:40:59 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdarg.h>
 
-int	ft_max_i(size_t arg_count, ...)
+int	ft_max(size_t arg_count, ...)
 {
 	size_t	i;
 	va_list	args;
@@ -26,6 +26,27 @@ int	ft_max_i(size_t arg_count, ...)
 	while (i < (arg_count - 1))
 	{
 		curr_val = va_arg(args, int);
+		if (curr_val > max_val)
+			max_val = curr_val;
+		i++;
+	}	
+	va_end(args);
+	return (max_val);
+}
+
+double	ft_fmax(size_t arg_count, ...)
+{
+	size_t	i;
+	va_list	args;
+	double	curr_val;
+	double	max_val;
+
+	i = 0;
+	va_start(args, arg_count);
+	max_val = va_arg(args, double);
+	while (i < (arg_count - 1))
+	{
+		curr_val = va_arg(args, double);
 		if (curr_val > max_val)
 			max_val = curr_val;
 		i++;
