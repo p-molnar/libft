0cc78338f6f2d900f66ffc0c9e9f77409730d018
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-11 20:47:38 +0200

    merge min functions into one file

--- src/math/ft_min.c
+++ src/math/ft_min.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_min_i.c                                         :+:    :+:            */
+/*   ft_min.c                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/07 18:34:03 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/06/01 15:47:23 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/11 20:41:43 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdarg.h>
 
-int	ft_min_i(size_t arg_count, ...)
+int	ft_min(size_t arg_count, ...)
 {
 	size_t	i;
 	va_list	args;
@@ -26,6 +26,27 @@ int	ft_min_i(size_t arg_count, ...)
 	while (i < (arg_count - 1))
 	{
 		curr_val = va_arg(args, int);
+		if (curr_val < min_val)
+			min_val = curr_val;
+		i++;
+	}	
+	va_end(args);
+	return (min_val);
+}
+
+double	ft_fmin(size_t arg_count, ...)
+{
+	size_t	i;
+	va_list	args;
+	double	curr_val;
+	double	min_val;
+
+	i = 0;
+	va_start(args, arg_count);
+	min_val = va_arg(args, double);
+	while (i < (arg_count - 1))
+	{
+		curr_val = va_arg(args, double);
 		if (curr_val < min_val)
 			min_val = curr_val;
 		i++;
