6f861082283c0c721acc0119eafaa1eba0dcde08
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-24 14:05:44 +0200

    Add file

---
+++ src/memory/ft_realloc.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_realloc.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/16 13:40:27 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/04/24 14:03:01 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+#include <stdlib.h>
+
+void	*ft_realloc(void *obj, size_t obj_size,
+	size_t curr_mem_size, size_t new_size)
+{
+	void	*new_ptr;
+
+	new_ptr = ft_calloc(new_size, new_size);
+	if (obj == NULL)
+		return (new_ptr);
+	if (new_size == 0)
+		new_ptr = ft_memcpy(new_ptr, obj, curr_mem_size);
+	else
+	{
+		if (new_size <= curr_mem_size * obj_size)
+			ft_memcpy(new_ptr, obj, new_size);
+		else
+			ft_memcpy(new_ptr, obj, curr_mem_size);
+	}
+	free(obj);
+	return (new_ptr);
+}
