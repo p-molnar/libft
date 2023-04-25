8fb8932590e4e88b5ae5fc5e195e9b7139795b4e
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-25 09:41:39 +0200

    Rework function

--- src/memory/ft_realloc.c
+++ src/memory/ft_realloc.c
@@ -6,29 +6,28 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/16 13:40:27 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/25 09:30:28 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/25 09:41:26 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <stdlib.h>
 
-void	*ft_realloc(void *obj, size_t obj_size,
-	size_t curr_mem_size, size_t new_size)
+void	*ft_realloc(void *obj, size_t obj_size, size_t new_size)
 {
 	void	*new_ptr;
 
-	new_ptr = ft_calloc(new_size, obj_size);
+	new_ptr = malloc(obj_size);
 	if (obj == NULL)
 		return (new_ptr);
 	if (new_size == 0)
-		new_ptr = ft_memcpy(new_ptr, obj, curr_mem_size);
+		new_ptr = ft_memcpy(new_ptr, obj, obj_size);
 	else
 	{
-		if (new_size <= curr_mem_size * obj_size)
-			ft_memcpy(new_ptr, obj, new_size * obj_size);
+		if (new_size <= obj_size)
+			ft_memcpy(new_ptr, obj, new_size);
 		else
-			ft_memcpy(new_ptr, obj, curr_mem_size);
+			ft_memcpy(new_ptr, obj, obj_size);
 	}
 	free(obj);
 	return (new_ptr);
